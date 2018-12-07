14810763ed1bbea120c47101d6c12dbf6edf9e36
Ajay Bansal <ajaybansalq@gmail.com>
2018-12-07 12:58:26 +0530

    remove file from Lamp(7Dec2017)

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -82,9 +82,7 @@ check_fileServerType_param $fileServerType
 
   # Lamp requirements
   sudo apt-get install -y graphviz aspell php-soap php-json php-redis php-bcmath php-gd php-pgsql php-mysql php-xmlrpc php-intl php-xml php-bz2
-  if [ "$dbServerType" = "mssql" ]; then
-    install_php_mssql_driver
-  fi
+ 
 
   # PHP Version
   PhpVer=$(get_php_version)
